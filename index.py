b997fd6b72cec082504b9316d7359eaa5f1fc7be
Diego Braun <diegobraun@localhost.localdomain>
2018-06-19 18:06:01 -0300

    Prática estrutura de dados = dicionário

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
-lista = []
-lista.append("Python")
-lista.append("Java")
-lista.append("JavaScript")
-lista.append("PHP")
-print(lista)
+#lista = []
+#lista.append("Python")
+#lista.append("Java")
+#lista.append("JavaScript")
+#lista.append("PHP")
+#print(lista)
+#lista.reverse()
+#print(lista)#
+dic = {"chave1":"valor1",2:"valor2",(1,2):"valor3"}
+dic.items()
+print(dic)
