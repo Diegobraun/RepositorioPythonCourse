40eb19f822f17e470d6acce23e1035d911faa247
Diego Braun <diegobraun@localhost.localdomain>
2018-06-24 01:40:06 -0300

    Instalando Módulos

---
+++ installingModules.py
@@ -0,0 +1,7 @@
+#pypi.python.org
+#installing lib request 2.10.0
+#On terminal
+#pip install requests -> isso irá fazer uma busca no repositorio do git e irá adicionar a lib no python
+# a lib requests obviamente é necessária para realizar requisições
+#pip install virtualenv -> fará uma busca no repositório do git e adicionará ao lib
+#virtualenv serve para fazer ambiente virtuais isolados
