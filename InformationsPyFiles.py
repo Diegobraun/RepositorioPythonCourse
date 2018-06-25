ab31ad8647dae1da84e10b1ef9da823efa60d9e9
Diego Braun <diegobraun@localhost.localdomain>
2018-06-25 20:47:25 -0300

    Python django informations

---
+++ InformationsPyFiles.py
@@ -0,0 +1,6 @@
+#file __pycache__ - O arquivo guarda justamente todo o cache de uma aplicação, assim, sendo útil por si só
+#file __init__py - O arquivo guarda o 'init' ou seja, o inicio do arquivo
+#file setting.py - O arquivo guarda todas as configurações da aplicação e projeto, como databases e links uteis, como ligações com css e js
+#file url.py - O arquivo guarda urls uteis para a aplicação, como patterns, por exemplo.
+#file wsgi.py - O arquivo é utilizado para deploy com webservice.
+
